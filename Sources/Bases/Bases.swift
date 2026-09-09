1b92f6b619efd395733f94f6649c590b134173e9
Brandon <btoms.20@gmail.com>
2026-09-08 18:10:11 -0700

    introduced an unbrella module Bases that brings in all of the modules in one import statement

---
+++ Sources/Bases/Bases.swift
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2026 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+//
+//  Bases.swift
+//
+//  An umbrella module that re-exports every module in the package, so you can write
+//  `import Bases` instead of importing each base module/target individually.
+//
+
+@_exported import Base2
+@_exported import Base32
+@_exported import Base64
+@_exported import Base8
+@_exported import BaseX
+@_exported import BasesCore
